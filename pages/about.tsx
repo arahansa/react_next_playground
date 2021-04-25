f4368ae42a824c675992365be3615c7e6d8028fe
arahansa <arahansa@naver.com>
2021-04-25 23:33:07 +0900

    add formik

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
@@ -10,6 +10,38 @@ const AboutPage = () => (
         <a>Go home</a>
       </Link>
     </p>
+      <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={values => {
+              const errors : any = {};
+              if (!values.email) {
+                  errors.email = 'Required';
+              } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                  errors.email = 'Invalid email address';
+              }
+              return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+              }, 400);
+          }}
+      >
+          {({ isSubmitting }) => (
+              <Form>
+                  <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+                  <button type="submit" disabled={isSubmitting}>
+                      Submit
+                  </button>
+              </Form>
+          )}
+      </Formik>
   </Layout>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { Formik } from 'formik';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -9,6 +10,59 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+      <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={values => {
+              const errors: any = {};
+              if (!values.email) {
+                  errors.email = 'Required';
+              } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                  errors.email = 'Invalid email address';
+              }
+              return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+              }, 400);
+          }}
+      >
+          {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit}>
+                  <input
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                  />
+                  {errors.email && touched.email && errors.email}
+                  <input
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                  />
+                  {errors.password && touched.password && errors.password}
+                  <button type="submit" disabled={isSubmitting}>
+                      Submit
+                  </button>
+              </form>
+          )}
+      </Formik>
   </Layout>
 )
 
